a7a37c471c2a5a963bcba3503fbe7094bc1044aa
anitavkapari <anitavkapari@gmail.com>
2022-01-28 22:47:08 +0530

    Implementing bean life cycle using interfaces | IntializingBean | DisposableBean

--- src/main/java/com/springcore/lifcycle/Test.java
+++ src/main/java/com/springcore/lifcycle/Test.java
@@ -10,9 +10,14 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifcycle/configSamosa.xml");
+        context.registerShutdownHook();
 		Samosa samosa = (Samosa) context.getBean("samosa");
         System.out.println( samosa );
-        context.registerShutdownHook();
+       
+        System.out.println( "+++++++++++++++++++++++++++" );
+
+        Pepsi pepsi = (Pepsi) context.getBean("pepsi");
+        System.out.println( pepsi );
 	}
 
 }
